c48a66ceae1e428dee86a4164485aed45535c397
Lawerence E Mize Jr <lawerencem@lawerences-mbp.home>
2014-10-12 22:25:59 -0400

    NEON-1230: Adding a grunt task to generate a war file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,11 +29,37 @@ module.exports = function(grunt) {
           copy: false
         }
       }
+    },
+
+    /*
+     * Build a WAR (web archive) without Maven or the JVM installed.
+     */
+    war: {
+      target: {
+        options: {
+          war_dist_folder: "target",
+          war_verbose: true,
+          war_name: 'neon-gtd',
+          webxml_welcome: 'index.html',
+          webxml_display_name: 'Neon Geo Temporal Dashboard',
+          webxml_mime_mapping: [{ 
+            extension: 'woff', 
+            mime_type: 'application/font-woff' 
+          }]
+        },
+        files: [
+          {
+            expand: true,
+            cwd: ".",
+            src: ["app/**", "lib/**"],
+            dest: ""
+          }
+        ]
+      }
     }
-
-
   });
 
+  grunt.loadNpmTasks('grunt-war');
   grunt.loadNpmTasks('grunt-bower-task');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
