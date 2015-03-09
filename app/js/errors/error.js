55c4a669644749ef93fe05e26ec8833aab24190d
tschellenberg <thomas.schellenberg@nextcentury.com>
2015-03-09 11:02:13 -0400

    NEON-1355 Refactored and cleaned-up JS error handling utility code.  The stacktrace is now displayed is the same notification as the error message due to unavoidable behavior in the notifyjs library.

--- app/js/errors/error.js
+++ app/js/errors/error.js
@@ -17,42 +17,23 @@
 
 var error = error || {};
 
-error.UNKNOWN_ERROR_MESSAGE = "An error occurred.";
-
-// Store the stack trace text for each individual error so we don't have to insert it into the HTML.
-// We'll delete it when we're finished with it.
-error.STACK_TRACE_STORAGE = {};
-error.STACK_TRACE_NEXT_INDEX = 1;
-
 $.notify.addStyle("error", {
     html:
         // The first div gets reclassed by notifyjs.
         "<div>" +
-            // Store the stack trace index corresponding to this error.
-            "<span data-notify-text='index' class='index hidden'></span>" +
             // Add the notifyjs bootstrap styling.
             "<div class='notifyjs-bootstrap-base notifyjs-bootstrap-error'>" +
                 "<div class='message' data-notify-text='message'/>" +
                 "<div class='buttons'>" +
-                    "<button class='btn btn-default pull-right' onclick=\"error.showStackTraceNotification_(this.parentElement.parentElement.parentElement);\">" +
+                    "<button class='btn btn-default pull-right' onclick=\"error.toggleStacktrace_(this.parentElement.parentElement);\">" +
                         "Details" +
                     "</button>" +
-                    "<button class='btn btn-default pull-right' onclick=\"error.hideErrorNotification_(this.parentElement.parentElement.parentElement);\">" +
+                    "<button class='btn btn-default pull-right' onclick=\"error.hideError_(this.parentElement.parentElement);\">" +
                         "OK" +
                     "</button>" +
                     "<div class='clearfix'></div>" +
                 "</div>" +
-            "</div>" +
-        "</div>"
-});
-
-$.notify.addStyle("stacktrace", {
-    html:
-        // The first div gets reclassed by notifyjs.
-        "<div>" +
-            // Add the notifyjs bootstrap styling.
-            "<div class='notifyjs-bootstrap-base notifyjs-bootstrap-error'>" +
-                "<div class='stacktrace well' data-notify-html='stacktrace'/>" +
+                "<div class='stacktrace well' data-notify-html='stacktrace' style='display: none;'></div>" +
             "</div>" +
         "</div>"
 });
@@ -64,11 +45,8 @@ $.notify.addStyle("stacktrace", {
  * @param {String} The error stack trace.
  * @return The jQuery element for the error message.
  */
-error.showErrorMessage = function(htmlElement, message, stackTrace) {
+error.showErrorMessage = function(htmlElement, message, stacktrace) {
     var jqueryElement = $(htmlElement);
-
-    var index = error.STACK_TRACE_NEXT_INDEX++;
-    error.STACK_TRACE_STORAGE[index] = stackTrace;
 
     // Show a temporary notification in the corner of the dashboard.
     $.notify(message);
@@ -76,7 +54,7 @@ error.showErrorMessage = function(htmlElement, message, stackTrace) {
     // Show a notification in the visualization itself.
     $.notify(jqueryElement, {
         message: message,
-        index: index
+        stacktrace: stacktrace.replace(/\n/g, "<br/>").replace(/\t/g, "&nbsp; ")
     }, {
         style: "error",
         clickToHide: false,
@@ -85,7 +63,7 @@ error.showErrorMessage = function(htmlElement, message, stackTrace) {
     });
 
     // The notification is added as a sibling to the input element.
-    var errorNotification = jqueryElement.parent().find(".notifyjs-error-base");
+    var errorNotification = jqueryElement.parent().find(".notifyjs-container");
 
     error.resizeErrorNotification_(jqueryElement, errorNotification);
 
@@ -93,18 +71,16 @@ error.showErrorMessage = function(htmlElement, message, stackTrace) {
 };
 
 error.resizeErrorNotification_ = function(jqueryElement, errorNotification) {
-    var errorNotificationContainer = errorNotification.parent();
-
     // Resize the notification so its width matches the jqueryElement.  Do this before calculating its height due to wrapping.
     var cssWidth = jqueryElement.outerWidth(true);
-    errorNotificationContainer.css("width", cssWidth + "px");
+    errorNotification.css("width", cssWidth + "px");
 
     // The notifyjs container element is hidden until its animation finishes so use its child to get its dimensions.
-    var cssTop = jqueryElement.outerHeight(true) - errorNotification.outerHeight(true);
+    var cssTop = jqueryElement.outerHeight(true) - errorNotification.find(".notifyjs-error-base").outerHeight(true);
 
     // The 'top' style of the notifyjs container element gets set by notifyjs to position the notification outside the
     // bottom of the input element.  We instead want to position it inside the bottom of the input element.
-    errorNotificationContainer.css("top", cssTop + "px");
+    errorNotification.css("top", cssTop + "px");
 };
 
 /**
@@ -112,53 +88,17 @@ error.resizeErrorNotification_ = function(jqueryElement, errorNotification) {
  * @param {Object} The jQuery element for the error message.
  */
 error.hideErrorMessage = function(errorNotification) {
-    // Hide the corresponding stack trace notification first if it exists.
-    var stackTraceNotification = errorNotification.parent().find(".notifyjs-stacktrace-base");
-    stackTraceNotification.trigger("notify-hide");
+    var stacktrace = errorNotification.find(".stacktrace");
+
+    if(stacktrace.css("display") !== "none") {
+        stacktrace.slideUp();
+    }
+
     errorNotification.trigger("notify-hide");
 }
 
-error.hideErrorNotification_ = function(htmlElement) {
+error.hideError_ = function(htmlElement) {
     error.hideErrorMessage($(htmlElement));
-};
-
-error.showStackTraceNotification_ = function(htmlElement) {
-    var errorNotification = $(htmlElement);
-
-    if(errorNotification.find(".notifyjs-stacktrace-base").length) {
-        return;
-    }
-
-    var index = errorNotification.find(".index").html();
-    var stackTrace = error.STACK_TRACE_STORAGE[index].replace(/\n/g, "<br/>").replace(/\t/g, "");
-
-    // Position the stack trace notification directly below the error notification.
-    $.notify(errorNotification.find(".notifyjs-bootstrap-base"), {
-        stacktrace: stackTrace
-    }, {
-        style: "stacktrace",
-        clickToHide: false,
-        autoHide: false,
-        arrowShow: false
-    });
-
-    error.resizeStackTraceNotification_(errorNotification);
-
-    // Clean up to save a bit of memory space since we won't need the stored stack trace anymore.
-    error.STACK_TRACE_STORAGE[index] = "";
-};
-
-error.resizeStackTraceNotification_ = function(errorNotification) {
-    var stackTraceNotificationContainer = errorNotification.find(".notifyjs-stacktrace-base").parent();
-
-    // Resize the stack trace message so its width matches the error message.
-    var cssWidth = errorNotification.find(".message").width();
-    stackTraceNotificationContainer.find(".well").css("width", cssWidth + "px");
-
-    // Ensure the stack trace notification is positioned directly below the error notification.
-    // This is relevant if resizing the widget causes the error notification to change in height.
-    var cssTop = errorNotification.height() + 5;
-    stackTraceNotificationContainer.css("top", cssTop + "px");
 };
 
 /**
@@ -168,5 +108,15 @@ error.resizeStackTraceNotification_ = function(errorNotification) {
  */
 error.resizeErrorMessage = function(htmlElement, errorNotification) {
     error.resizeErrorNotification_($(htmlElement), errorNotification);
-    error.resizeStackTraceNotification_(errorNotification);
+};
+
+error.toggleStacktrace_ = function(htmlElement) {
+    var stacktrace = $(htmlElement).find(".stacktrace");
+
+    if(stacktrace.css("display") !== "none") {
+        stacktrace.slideUp();
+    }
+    else {
+        stacktrace.slideDown();
+    }
 };
