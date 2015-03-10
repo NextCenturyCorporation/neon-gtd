77a2d57d1eae7cc70a656837c0941de3f36aea7e
tschellenberg <thomas.schellenberg@nextcentury.com>
2015-03-09 21:10:34 -0400

    NEON-1355 The Error Notification Service now uses the superior Bootstrap Notify library to display notifications instead of the limited notifyjs.  Error notifications are working correctly.

--- app/js/errors/error.js
+++ app/js/errors/error.js
@@ -16,120 +16,112 @@
  */
 
 angular.module("neonDemo.services")
-.factory("ErrorHandlingService",
-    function($rootScope, $compile) {
+.factory("ErrorNotificationService",
+    function() {
         var service = {};
 
-        $.notify.addStyle("error", {
-            html:
-                // The first div gets reclassed by notifyjs.
+        // In Bootstrap Notify, {0} = settings.type, {1} = options.title, {2} = options.message, {3} = options.url, {4} = options.target
+        var ERROR_AND_PROGRESS_BAR_TEMPLATE =
+            "<div data-notify='container' class='alert alert-{0} neon-error-global' role='alert'>" +
+                "<button type='button' aria-hidden='true' class='close' data-notify='dismiss'>&times;</button>" +
                 "<div>" +
-                    // Add the notifyjs bootstrap styling.
-                    "<div class='notifyjs-bootstrap-base notifyjs-bootstrap-error'>" +
-                        "<div class='message' data-notify-text='message'/>" +
-                        "<div class='buttons'>" +
-                            "<button class='btn btn-default pull-right' ng-click=\"toggleStacktrace_(this.parentElement.parentElement);\">" +
-                                "Details" +
-                            "</button>" +
-                            "<button class='btn btn-default pull-right' ng-click=\"hideError_(this.parentElement.parentElement);\">" +
-                                "OK" +
-                            "</button>" +
-                            "<div class='clearfix'></div>" +
-                        "</div>" +
-                        "<div class='stacktrace well' data-notify-html='stacktrace' style='display: none;'></div>" +
-                    "</div>" +
-                "</div>"
-        });
+                    "<span class='neon-error-icon' data-notify='icon'></span>" +
+                    "<span class='neon-error-message' data-notify='message'>{2}</span>" +
+                "</div>" +
+                "<div class='progress' data-notify='progressbar'>" +
+                    "<div class='progress-bar progress-bar-{0}' role='progressbar' aria-valuenow='0' aria-valuemax='100' style='width: 0%;'></div>" +
+                "</div>" +
+            "</div>";
+
+        var ERROR_AND_STACKTRACE_TEMPLATE =
+            "<div data-notify='container' class='alert alert-{0} neon-error-element' role='alert'>" +
+                "<button type='button' aria-hidden='true' class='close' data-notify='dismiss'>&times;</button>" +
+                "<div>" +
+                    "<span class='neon-error-icon' data-notify='icon'></span>" +
+                    "<span class='neon-error-message' data-notify='message'>{2}</span>" +
+                "</div>" +
+                "<div class='neon-error-details'>Details <i class='glyphicon glyphicon-chevron-up'/></div>" +
+                // Use title here for the stacktrace because our options for terminology are limited.
+                "<div class='neon-error-stacktrace well' data-notify='title' style='display: none;'>{1}</div>" +
+            "</div>";
 
         /**
-         * Shows an error message popup for the given parameters.
-         * @param {HTMLElement} The notification will be displayed next to this element.
-         * @param {String} The error message that will be displayed in the notification.
-         * @param {String} The error stack trace.
-         * @return The jQuery element for the error message.
-         */
-        service.showErrorMessage = function(htmlElement, message, stacktrace) {
-            var jqueryElement = $(htmlElement);
-
-            // Show a temporary notification in the corner of the dashboard.
-            $.notify(message);
-
-            // Show a notification in the visualization itself.
-            $.notify(jqueryElement, {
-                message: message,
-                stacktrace: stacktrace.replace(/\n/g, "<br/>").replace(/\t/g, "&nbsp; ")
-            }, {
-                style: "error",
-                clickToHide: false,
-                autoHide: false,
-                arrowShow: false
-            });
-
-            // The notification is added as a sibling to the input element.
-            var errorNotification = jqueryElement.parent().find(".notifyjs-container");
-
-            // TODO Make this work!
-            // Initialize the angularjs directives inside the notification.
-            $compile(errorNotification.contents())($rootScope);
-
-            service.resizeError_(jqueryElement, errorNotification);
-
-            return errorNotification;
-        };
-
-        /**
-         * Resizes the error message popup and any associated stack trace popup.
-         * @param {HTMLElement} The resized element associated with the error message.
-         * @param {Object} The jQuery element for the error message.
-         */
-        service.resizeErrorMessage = function(htmlElement, errorNotification) {
-            service.resizeError_($(htmlElement), errorNotification);
-        };
-
-        service.resizeError_ = function(jqueryElement, errorNotification) {
-            // Resize the notification so its width matches the jqueryElement.  Do this before calculating its height due to wrapping.
-            var cssWidth = jqueryElement.outerWidth(true);
-            errorNotification.css("width", cssWidth + "px");
-
-            // The notifyjs container element is hidden until its animation finishes so use its child to get its dimensions.
-            var cssTop = jqueryElement.outerHeight(true) - errorNotification.find(".notifyjs-error-base").outerHeight(true);
-
-            // The 'top' style of the notifyjs container element gets set by notifyjs to position the notification outside the
-            // bottom of the input element.  We instead want to position it inside the bottom of the input element.
-            errorNotification.css("top", cssTop + "px");
-        };
-
-        /**
-         * Hides the given error message popup and any associated stack trace popup.
-         * @param {Object} The jQuery element for the error message.
+         * Hides the given error message notification.
+         * @param {Object} The jQuery element for the error message notification.
          */
         service.hideErrorMessage = function(errorNotification) {
-            var stacktrace = errorNotification.find(".stacktrace");
-
-            if(stacktrace.css("display") !== "none") {
-                stacktrace.slideUp();
-            }
-
-            errorNotification.trigger("notify-hide");
+            errorNotification.find('[data-notify="dismiss"]').trigger("click");
         }
 
-        service.hideError_ = function(htmlElement) {
-            service.hideErrorMessage($(htmlElement));
-        };
+        /**
+         * Shows two error message notifications (one temporary in the corner of the dashboard and the other inside a specified element).
+         * @param {HTMLElement} A notification will be displayed inside this element.
+         * @param {String} The error message that will be displayed in the notifications.
+         * @param {String} The error stacktrace.
+         * @return The jQuery element for the error message notification inside the input element.
+         */
+        service.showErrorMessage = function(element, message, stacktrace) {
+            service.hideErrorMessage($(element).find(".neon-error-element"));
 
-        service.toggleStacktrace_ = function(htmlElement) {
-            var stacktrace = $(htmlElement).find(".stacktrace");
+            // Display a temporary notification in the corner of the dashboard.
+            // Bootstrap Notify Format:  $.notify({ options }, { settings });
+            $.notify({
+                icon: "img/Error_512x512.png",
+                message: message
+            }, {
+                type: "danger",
+                offset: {
+                    x: 5,
+                    // Include space for the navbar.
+                    y: 55 
+                },
+                icon_type: "src",
+                template: ERROR_AND_PROGRESS_BAR_TEMPLATE
+            });
 
-            if(stacktrace.css("display") !== "none") {
-                stacktrace.slideUp();
-            } else {
-                stacktrace.slideDown();
-            }
-        };
+            // Display a notification in the bottom of the input element.
+            $.notify({
+                icon: "img/Error_512x512.png",
+                // The title property is a keyword that will be used in the template.
+                title: stacktrace.replace(/\n/g, "<br/>").replace(/\t/g, "&nbsp; "),
+                message: message
+            }, {
+                // Display inside the input element.
+                element: element,
+                type: "danger",
+                placement: {
+                    from: "bottom",
+                    align: "center"
+                },
+                offset: {
+                    x: 20,
+                    y: 0 
+                },
+                // Display over neon popover elements.
+                z_index: 1061,
+                // No timeout.
+                delay: 0,
+                icon_type: "src",
+                template: ERROR_AND_STACKTRACE_TEMPLATE
+            });
 
-        service.init = function() {
-            $rootScope.hideError_ = this.hideError_;
-            $rootScope.toggleStacktrace_ = this.toggleStacktrace_;
+            var elementErrorNotification = $(element).find(".neon-error-element");
+            var stacktrace = elementErrorNotification.find(".neon-error-stacktrace");
+
+            // Expand/collapse the stacktrace whenever the user clicks on Details.
+            elementErrorNotification.find(".neon-error-details").on("click", function(e) {
+                if(stacktrace.css("display") !== "none") {
+                    $(this).find("i").removeClass("glyphicon-chevron-down");
+                    $(this).find("i").addClass("glyphicon-chevron-up");
+                    stacktrace.slideUp();
+                } else {
+                    $(this).find("i").removeClass("glyphicon-chevron-up");
+                    $(this).find("i").addClass("glyphicon-chevron-down");
+                    stacktrace.slideDown();
+                }
+            });
+
+            return elementErrorNotification;
         };
 
         return service;
