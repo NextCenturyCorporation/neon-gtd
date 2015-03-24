f85f781f4c44836e5ee9ecc38333ec336b4606ca
Clark Dorman <clark.dorman@nextcentury.com>
2015-03-24 15:01:33 -0400

    Changes to make it work full screen

--- app/js/appConfig.js
+++ app/js/appConfig.js
@@ -44,8 +44,9 @@ DEMO_CONFIG.opencpu.useAlerts = false;
 // If the neon application is loaded elsewhere, this can be changed as in the following example:
 // neon.SERVER_URL = "http://localhost:8080/neon"
 neon.SERVER_URL = "/neon";
-neon.DIG_ENABLED = false;
-neon.DIG_SERVER = "http://localhost:9000";
+neon.DIG_ENABLED = true;
+//neon.DIG_SERVER = "http://localhost:9000";
+neon.DIG_SERVER = "http://10.1.93.199:8080";
 
 // Database information for the visualizations.
 neon.DATABASE_NAME = "test";
--- app/js/neonDemoController.js
+++ app/js/neonDemoController.js
@@ -49,14 +49,14 @@ angular.module('neonDemo.controllers')
     };
 
     $scope.gridsterOpts = {
-        columns: 6, // the width of the grid, in columns
+        columns: 12, // the width of the grid, in columns
         pushing: true, // whether to push other items out of the way on move or resize
         floating: true, // whether to automatically float items up so they stack (you can temporarily disable if you are adding unsorted items with ng-repeat)
         width: 'auto', // can be an integer or 'auto'. 'auto' scales gridster to be the full width of its containing element
         colWidth: 'auto', // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
         rowHeight: 'match', // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
         margins: [10, 10], // the pixel distance between each widget
-        outerMargin: false, // whether margins apply to outer edges of the grid
+        outerMargin: true, // whether margins apply to outer edges of the grid
         isMobile: false, // stacks the grid items if true
         mobileBreakPoint: 800, // if the screen is not wider that this, remove the grid layout and stack the items
         mobileModeEnabled: true, // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
