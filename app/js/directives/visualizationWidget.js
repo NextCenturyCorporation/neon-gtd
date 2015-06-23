0e973b60a918872451532a3978de8292af3144da
Neva Reed <neva.reed@nextcentury.com>
2015-06-23 12:08:09 -0400

    NEON-1574 Make close button configurable

--- app/js/directives/visualizationWidget.js
+++ app/js/directives/visualizationWidget.js
@@ -38,10 +38,10 @@ angular.module('neonDemo.directives').directive('visualizationWidget', ["config"
             gridsterConfigIndex: "="
         },
         template: '<div class="visualization-drag-handle"><div class="visualization-buttons">' +
-                '<a class="btn" ng-click="toggleSize()"" ng-mouseover="$event.stopPropagation()">' +
+                '<a class="btn" ng-click="toggleSize()" ng-class="{center: hideCloseButton}" ng-mouseover="$event.stopPropagation()">' +
                 '   <span  class="glyphicon" ng-class="(oldSize) ? \'glyphicon-resize-small\' : \'glyphicon-resize-full\'"></span>' +
                 '</a>' +
-                '<a class="btn" ng-click="remove()" ng-mouseover="$event.stopPropagation()">' +
+                '<a class="btn" ng-click="remove()" ng-hide="hideCloseButton" ng-mouseover="$event.stopPropagation()">' +
                 '   <span  class="glyphicon glyphicon-remove"></span>' +
                 '</a>' +
                 '</div></div>',
@@ -57,6 +57,9 @@ angular.module('neonDemo.directives').directive('visualizationWidget', ["config"
             }
             if(config.hideHeader) {
                 widgetElement.setAttribute("hide-header", true);
+            }
+            if(config.hideCloseButton) {
+                $scope.hideCloseButton = config.hideCloseButton;
             }
 
             // Pass along any bindings.
