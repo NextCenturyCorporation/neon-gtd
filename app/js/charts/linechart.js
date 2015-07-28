2d59a95ba1d2666c21df2fcd56864fa0393acc71
tschellenberg <thomas.schellenberg@nextcentury.com>
2015-07-28 11:06:16 -0400

    Fixed a minor bug in the highlight behavior in the linechart and timeline.

--- app/js/charts/linechart.js
+++ app/js/charts/linechart.js
@@ -257,8 +257,8 @@ charts.LineChart.prototype.selectDate = function(startDate, endDate) {
     }
 
     // If the start or end date is outside the date range of the data, set it to the of the start (inclusive) or end (exclusive) index of the data.
-    startIndex = startDate < dataStartDate ? 0 : startIndex;
-    endIndex = endDate > dataEndDate ? dataLength : endIndex;
+    startIndex = startDate <= dataStartDate ? 0 : startIndex;
+    endIndex = endDate >= dataEndDate ? dataLength : endIndex;
 
     if(startIndex < 0 || endIndex < 0 || endDate < dataStartDate || startDate > dataEndDate) {
         this.deselectDate();
