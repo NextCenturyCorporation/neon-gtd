8c88f523676b6cd9730446b348fa8698a2ef9e27
tschellenberg <thomas.schellenberg@nextcentury.com>
2016-01-27 18:10:04 -0500

    Whoops!  Fixed a silly mistake in a recent bug-fix for the timeline chart.

--- app/js/charts/timelineSelectorChart.js
+++ app/js/charts/timelineSelectorChart.js
@@ -241,7 +241,7 @@ charts.TimelineSelectorChart = function(element, configuration) {
         var extentWidth = brushElement.find('.extent').attr('width');
         var width = parseInt(brushElement.find('.mask-west').attr('width').replace('px', ''), 10);
 
-        if(parseFloat(xPos) + parseFloat(extentWidth) < 0 || parseFloat(xPos) > 0) {
+        if(parseFloat(xPos) + parseFloat(extentWidth) < 0 || parseFloat(xPos) > width) {
             xPos = 0;
             extentWidth = 0;
             width = 0;
